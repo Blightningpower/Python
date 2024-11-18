0541cbf1e0010ec4af93ca53dce86a54be4529b9
Bavli Armanyous <bavli898@gmail.com>
2024-11-18 04:17:21 +0100

    Best veel geleerd.

--- FlaskIntroduction/app.py
+++ FlaskIntroduction/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from models import db, Task
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db.init_app(app)
 
-@app.route('/')
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return "Hello"
     return render_template('index.html')
 
 if __name__ == "__main__":
